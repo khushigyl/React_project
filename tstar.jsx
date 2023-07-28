176ad4a8b423c26050f1353231bdb9d77ea9a46f
Arpita389 <138769882+Arpita389@users.noreply.github.com>
2023-07-28 22:12:23 +0530

    Add files via upload

---
+++ tstar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Tstar() {
+  return (
+    <div className="stars">
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star"></i>
+      <i className="fas fa-star-half-alt"></i>
+    </div>
+  );
+}
+export default Tstar;
